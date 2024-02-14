1762f82a4c2008f9046d5823f0e6b880c9bb747b
EC2 Default User <ec2-user@ip-172-31-31-85.ap-southeast-1.compute.internal>
2024-02-14 11:11:43 +0000

    terraform-file

---
+++ elb.tf
@@ -0,0 +1,27 @@
+ resource "aws_elb" "bar" {
+  name                      = "shruthi-terraform-elb"
+  availability_zones        = ["ap-southeast-1a", "ap-southeast-1b"]
+
+  listener {
+    instance_port          = 80
+    instance_protocol      = "http"
+    lb_port                = 80
+    lb_protocol            = "http"
+  }
+ 
+  health_check {
+    healthy_threshold    = 2
+    unhealthy_threshold  = 2
+    timeout              = 5
+    target               = "HTTP:80/"
+    interval             = 30
+  }
+
+  instances                 = ["${aws_instance.one.id}","${aws_instance.two.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout              = 400
+  tags = {   
+    Name = "shruthi-tf-elb"
+  }
+}
+
